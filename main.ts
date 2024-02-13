8704e77dc157ecd3c4d34cc2285d631038a27569
gnietto <elcirculoti@gmail.com>
2024-02-13 12:45:34 -0300

    feat: agrega estructura HTML5 a cada ruta

--- main.ts
+++ main.ts
@@ -1,45 +1,81 @@
 import { Hono } from "https://deno.land/x/hono@v4.0.0/mod.ts";
-import { html, css, Style } from "https://deno.land/x/hono/helper.ts";
+import { html } from "https://deno.land/x/hono/helper.ts";
 
 const app = new Hono();
 
-const Encabezado = (titulo, descripcion) => html`
+const vistaHtml = (componente1, componente2) =>
+  html`
+  <!DOCTYPE html>
+  <html>
+    <head>
+    </head> 
+    <body>
+      <div>
+        ${componente1}
+      </div>
+      <div>
+        ${componente2}
+      </div>
+    </body>
+  </html>
+`;
+
+const Encabezado = (titulo, descripcion) =>
+  html`
   <header>
     <h1> ${titulo} </h1>
     <p> ${descripcion} </p>
   </header>
-`
+`;
+
+const Cuerpo = (placeholder) => html`
+  <main>
+    ${placeholder}
+  </main>
+`;
 
 app.get("/", (c) => {
-  const props = {
-    titulo:"Gamma Tech and Life",
-    descripcion: "Iniciativa comercial"
-  }
-  return c.html(Encabezado(props.titulo, props.descripcion))
+  const args = {
+    titulo: "Gamma Tech and Life",
+    descripcion: "Iniciativa comercial",
+    placeholder: "Este es un contenedor para el cuerpo de cada seccion del showcase",
+    Encabezado,
+    Cuerpo
+  };
+  return c.html(vistaHtml(args.Encabezado(args.titulo, args.descripcion), args.Cuerpo(args.placeholder)));
 });
 
 app.get("/dashboard", (c) => {
-  const props = {
+  const args = {
     titulo:"Dashboard",
-    descripcion: "Descripcion gráfica de los datos actuales de alguna API"
-  }
-  return c.html(Encabezado(props.titulo, props.descripcion))
+    descripcion: "Descripcion gráfica de los datos actuales de alguna API",
+    placeholder: "Este es un contenedor para el cuerpo de cada seccion del showcase",
+    Encabezado,
+    Cuerpo
+  };
+  return c.html(vistaHtml(args.Encabezado(args.titulo, args.descripcion), args.Cuerpo(args.placeholder)));
 });
 
 app.get("/frutakids", (c) => {
-  const props = {
+  const args = {
     titulo:"Frutakids",
-    descripcion: "Juego educativo para niños"
+    descripcion: "Juego educativo para niños",
+    placeholder: "Este es un contenedor para el cuerpo de cada seccion del showcase",
+    Encabezado,
+    Cuerpo
   }
-  return c.html(Encabezado(props.titulo, props.descripcion))
+  return c.html(vistaHtml(args.Encabezado(args.titulo, args.descripcion), args.Cuerpo(args.placeholder)));
 });
 
 app.get("/horoscopo", (c) => {
-  const props = {
+  const args  = {
     titulo:"Horóscopo",
-    descripcion: "Predicciones astrológicas de algún tipo"
+    descripcion: "Predicciones astrológicas de algún tipo",
+    placeholder: "Este es un contenedor para el cuerpo de cada seccion del showcase",
+    Encabezado,
+    Cuerpo
   }
-  return c.html(Encabezado(props.titulo, props.descripcion))
+  return c.html(vistaHtml(args.Encabezado(args.titulo, args.descripcion), args.Cuerpo(args.placeholder)));
 });
 
 Deno.serve(app.fetch);
