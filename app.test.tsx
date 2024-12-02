3ce8da46ac2c9f3f4eb40833c9813d599147e390
gnietto <elcirculoti@gmail.com>
2024-12-02 09:59:12 -0300

    test: simplifica la estructura de tests
    Este commit simplifica las pruebas automáticas eliminando la ejecución
    de tests en pasos de Deno (t.step()) por representar una adición
    innecesaria para el actual proyecto de software web.

--- app.test.tsx
+++ app.test.tsx
@@ -3,36 +3,28 @@ import { expect } from "jsr:@std/expect";
 import { app } from "./app.tsx";
 
 Deno.test("El servidor funciona correctamente", async (t) => {
-  await t.step("responde con status 200", async () => {
     const res = await app.request("/");
     expect(res.status).toBe(200);
-  });
 });
 
 Deno.test("La página html está en modo estándar en vez de modo quirks", async (t) => {
-  await t.step("La página html tiene doctype correcto", async (t) => {
     const res = await app.request("/");
     const contieneTexto = await res.text();
     expect(contieneTexto).toContain("<!doctype html>");
-  });
 });
 
 Deno.test("<Presentacion /> despliega contenido", async (t) => {
-  await t.step("<Presentacion /> despliega palabras clave", async (t) => {
     const res = await app.request("/");
     const contieneTexto = await res.text();
     expect(contieneTexto).toContain("Spaceger");
     expect(contieneTexto).toContain("Agile");
     expect(contieneTexto).toContain("DevOps");
     expect(contieneTexto).not.toContain("Germ");
-  });
 });
 
 Deno.test("<Footer /> despliega contenido", async (t) => {
-  await t.step("<Footer/> despliega palabras clave", async (t) => {
     const res = await app.request("/");
     const contieneTexto = await res.text();
     expect(contieneTexto).toContain("Diseñado");
     expect(contieneTexto).toContain("Spaceger");
-  });
 });
